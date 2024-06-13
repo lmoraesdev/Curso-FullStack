8d557b9387bc9c99e1776b0d97fcc10ff16fd0c0
lmoraesdev <lbc_moraes@outlook.com>
2024-06-12 22:00:01 -0300

    feat: exercise 5,6 and 7

---
+++ programming-logic/exercise5.js
@@ -0,0 +1,12 @@
+// Escreve um função que recebe 2 números e retorna o maior deles.
+
+// function maiorNumero(num1, num2) {
+//   return num1 > num2 ? num1 : num2;
+// }
+
+// const maiorNumero = (num1, num2) => {
+//   return num1 > num2 ? num1 : num2;
+// };
+
+const maiorNumero = (num1, num2) => (num1 > num2 ? num1 : num2);
+console.log(maiorNumero(50, 10));
