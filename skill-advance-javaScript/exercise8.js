df20976d3971b6d6a7fdf4f8667572a28a8f2805
lmoraesdev <lbc_moraes@outlook.com>
2024-07-10 15:10:18 -0300

    feat: estudo de map

---
+++ skill-advance-javaScript/exercise8.js
@@ -0,0 +1,30 @@
+// Dobre os Números
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numerosEmDobro = numeros.map((valor) => valor * 2);
+console.log(numerosEmDobro);
+
+//Para cada elemento:
+//Retorne apenas uma string com o nome da pessoa
+//Remova apenas a chave "nome" do objeto
+//Adicione uma chave id em cada objetivo
+const pessoas = [
+  { nome: 'Luiz', idade: 62 },
+  { nome: 'Maria', idade: 23 },
+  { nome: 'Eduardo', idade: 55 },
+  { nome: 'Letícia', idade: 19 },
+  { nome: 'Rosana', idade: 32 },
+  { nome: 'Wallace', idade: 47 },
+];
+
+const nomes = pessoas.map((obj) => obj.nome);
+console.log(nomes);
+
+const idades = pessoas.map((obj) => ({ idade: obj.idade }));
+console.log(idades);
+
+const comIds = pessoas.map(function (obj, indice) {
+  const newObj = { ...obj };
+  newObj.id = indice;
+  return newObj;
+});
+console.log(comIds);
