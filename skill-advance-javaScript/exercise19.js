9042f8c80c924aab0d05a535856b61bf389a3c1f
lmoraesdev <lbc_moraes@outlook.com>
2024-07-22 13:14:57 -0300

    feat: estudo sobre prototype

---
+++ skill-advance-javaScript/exercise19.js
@@ -0,0 +1,57 @@
+// Superclass
+function Conta(agencia, conta, saldo) {
+  this.agencia = agencia;
+  this.conta = conta;
+  this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function (valor) {
+  if (this.saldo < valor) {
+    console.log(`Saldo Insuficiente: ${this.saldo}`);
+    return;
+  }
+  this.saldo -= valor;
+  this.verSaldo();
+};
+
+Conta.prototype.depositar = function (valor) {
+  this.saldo += valor;
+  this.verSaldo();
+};
+
+Conta.prototype.verSaldo = function () {
+  console.log(
+    `Agencia/conta: ${this.agencia}/${
+      this.conta
+    } | Saldo: R$ ${this.saldo.toFixed(2)}`
+  );
+};
+
+function CC(agencia, conta, saldo, limite) {
+  Conta.call(this, agencia, conta, saldo);
+  this.limite = limite;
+}
+
+CC.prototype = Object.create(Conta.prototype);
+CC.prototype.constructor = CC;
+
+CC.prototype.sacar = function (valor) {
+  if (valor > this.saldo + this.limite) {
+    console.log(`Saldo Insuficiente: ${this.saldo}`);
+    return;
+  }
+  this.saldo -= valor;
+  this.verSaldo();
+};
+
+function CP(agencia, conta, saldo) {
+  Conta.call(this, agencia, conta, saldo);
+}
+
+CP.prototype = Object.create(Conta.prototype);
+CP.prototype.constructor = CP;
+
+const cp = new CP(12, 33, 0);
+cp.depositar(10);
+cp.sacar(10);
+cp.sacar(1);
